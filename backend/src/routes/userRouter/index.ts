ea374aa78b34cc38e3e278fe593c395efd8753ad
fabio <fabiojuvenalpereira@hotmail.com>
2022-10-16 22:15:26 -0300

    feat: :sparkles: add middleware on the route to validate user token

--- backend/src/routes/userRouter/index.ts
+++ backend/src/routes/userRouter/index.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 
 import * as userController from '../../controllers/usersController/users.controller';
+import validateToken from '../../middlewares/validateToken.midleware';
 
 const userRouter = Router();
 
 userRouter.get('/', userController.getAllUsers);
-userRouter.get('/:id', userController.getUserById);
+userRouter.get('/:id', validateToken, userController.getUserById);
 
 userRouter.post('/', userController.createUser);
 userRouter.post('/login', userController.userLogin);
 
-userRouter.delete('/:id', userController.deleteUser);
+userRouter.delete('/:id', validateToken, userController.deleteUser);
 
 export default userRouter;
