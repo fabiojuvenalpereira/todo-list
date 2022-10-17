2e45bf3a90b44b0bff6f5b875af20940073306d7
fabio <fabiojuvenalpereira@hotmail.com>
2022-10-16 22:23:58 -0300

    feat: :sparkles: update taks controller layer

--- backend/src/controllers/tasksController/tasks.controller.ts
+++ backend/src/controllers/tasksController/tasks.controller.ts
@@ -1,52 +1,70 @@
 import { NextFunction, Response, Request } from 'express';
 import * as tasksServices from '../../services/tasksServices/tasks.service';
 
-export async function getAllTasks(request: Request, response: Response, next: NextFunction) {
+export async function createTask(request: Request, response: Response, next: NextFunction) {
   try {
-    const result = await tasksServices.getAllTasks();
+    const data = request.body;
+    const result = await tasksServices.createTask(data);
 
     return response.status(result.status).json(result.content);
-  } catch (error: any) {
+  } catch (error) {
+    return next(error);
+  }
+}
+
+export async function getAllTasks(request: Request, response: Response, next: NextFunction) {
+  try {
+    const { userId } = request.params;
+    const result = await tasksServices.getAllTasks(userId);
+
+    return response.status(result.status).json(result.content);
+  } catch (error) {
     return next(error);
   }
 }
 
 export async function getTaskById(request: Request, response: Response, next: NextFunction) {
   try {
-    const result = await tasksServices.getTaskById();
+    const { id } = request.params;
+    const result = await tasksServices.getTaskById(id);
 
     return response.status(result.status).json(result.content);
-  } catch (error: any) {
+  } catch (error) {
     return next(error);
   }
 }
 
 export async function updateTaskById(request: Request, response: Response, next: NextFunction) {
   try {
-    const result = await tasksServices.updateTaskById();
+    const { id } = request.params;
+    const data = request.body;
+
+    const result = await tasksServices.updateTaskById(id, data);
 
     return response.status(result.status).json(result.content);
-  } catch (error: any) {
+  } catch (error) {
     return next(error);
   }
 }
 
 export async function deleteTaskById(request: Request, response: Response, next: NextFunction) {
   try {
-    const result = await tasksServices.deleteTaskById();
+    const { id } = request.params;
+    const result = await tasksServices.deleteTaskById(id);
 
     return response.status(result.status).json(result.content);
-  } catch (error: any) {
+  } catch (error) {
     return next(error);
   }
 }
 
 export async function deleteAllTasks(request: Request, response: Response, next: NextFunction) {
   try {
-    const result = await tasksServices.deleteAllTasks();
+    const { userId } = request.params;
+    const result = await tasksServices.deleteAllTasksByUser(userId);
 
     return response.status(result.status).json(result.content);
-  } catch (error: any) {
+  } catch (error) {
     return next(error);
   }
 }
