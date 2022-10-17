50e7e6472d58e1316df445c694e0b9e852783764
fabio <fabiojuvenalpereira@hotmail.com>
2022-10-16 22:12:38 -0300

    fix: :pencil2: fixes the controller layer to operate as designed

--- backend/src/controllers/usersController/users.controller.ts
+++ backend/src/controllers/usersController/users.controller.ts
@@ -4,49 +4,54 @@ import * as userService from '../../services/userServices/users.services';
 
 export async function createUser(request: Request, response: Response, next: NextFunction) {
   try {
-    const result = await userService.createUser();
+    const data = request.body;
+    const result = await userService.createUser(data);
 
     return response.status(result.status).json(result.content);
-  } catch (error: any) {
+  } catch (error) {
     return next(error);
   }
 }
 
-export async function getAllUsers(request: Request, response: Response, next: NextFunction) {
+export async function getAllUsers(_request: Request, response: Response, next: NextFunction) {
   try {
     const result = await userService.getAllUsers();
 
     return response.status(result.status).json(result.content);
-  } catch (error: any) {
+  } catch (error) {
     return next(error);
   }
 }
 
 export async function getUserById(request: Request, response: Response, next: NextFunction) {
   try {
-    const result = await userService.getUserById();
+    const { id } = request.params;
+    const result = await userService.getUserById(id);
 
     return response.status(result.status).json(result.content);
-  } catch (error: any) {
+  } catch (error) {
     return next(error);
   }
 }
 
 export async function userLogin(request: Request, response: Response, next: NextFunction) {
   try {
-    const result = await userService.userLogin();
+    const { email, password } = request.body;
+    const result = await userService.userLogin(email, password);
 
     return response.status(result.status).json(result.content);
-  } catch (error: any) {
+  } catch (error) {
     return next(error);
   }
 }
 
 export async function deleteUser(request: Request, response: Response, next: NextFunction) {
   try {
-    const result = await userService.deleteUser();
+    const { id } = request.params;
+    const result = await userService.deleteUserId(id);
+
     return response.status(result.status).json(result.content);
-  } catch (error: any) {
+  } catch (error) {
     return next(error);
   }
 }
