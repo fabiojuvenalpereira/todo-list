d585b9edd0cc9f7acf2a306179d833aa4fb60cb5
fabio <fabiojuvenalpereira@hotmail.com>
2022-10-18 04:54:04 -0300

    refactor:

--- frontend/src/contexts/auth/AuthContext.jsx
+++ frontend/src/contexts/auth/AuthContext.jsx
@@ -1,8 +1,5 @@
 import { createContext } from 'react';
 
-const AuthContext = createContext({
-  user: Boolean,
-  login: () => {},
-});
+const AuthContext = createContext({});
 
 export default AuthContext;
