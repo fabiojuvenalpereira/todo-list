45cdb7d366bae3b3c23ad6bca80d9f4a78a555c4
fabio <fabiojuvenalpereira@hotmail.com>
2022-10-17 21:06:30 -0300

    feat: :sparkles: add route index

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index element={<Login />}/>
         <Route path='/tasks' element={<TaskList/>}/>
         <Route path='/login' element={<Login />}/>
         <Route path='/register' element={<Register />}/>
