97b31392e7844c7f195e7592484ec913baeea814
fabio <fabiojuvenalpereira@hotmail.com>
2022-10-18 23:11:23 -0300

    feat: :sparkles: add component logic

---
+++ frontend/src/components/list/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  min-height: 100vh;
+`;
+
+export const List = styled.ul`
+  list-style: none;
+`;
