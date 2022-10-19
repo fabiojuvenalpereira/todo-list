291f2310143af6e8eee2593f26fdc08b48bdaa2f
fabio <fabiojuvenalpereira@hotmail.com>
2022-10-18 23:23:45 -0300

    feat: :sparkles: add loading component

---
+++ frontend/src/components/loading/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from './styles';
+
+function Loading() {
+  return (
+    <Container>
+      <h1>Loading</h1>
+    </Container>
+  );
+}
+
+export default Loading;
